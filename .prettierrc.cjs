79e1e6a297d2ca8187253724969914a1a47bab2c
Valdir Rospirski <valdir.rospirski@gmail.com>
2023-09-27 10:12:46 -0300

    Finish

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,20 +1,20 @@
 const config = {
-    semi: false,
-    arrowParens: 'always',
-    singleQuote: true,
-    tabWidth: 4,
-    trailingComma: 'all',
-    printWidth: 100,
-    bracketSameLine: true,
-    plugins: ['prettier-plugin-astro'],
-    overrides: [
-        {
-            files: '*.astro',
-            options: {
-                parser: 'astro',
-            },
-        },
-    ],
+  semi: false,
+  arrowParens: 'always',
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 120,
+  bracketSameLine: true,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 }
 
 module.exports = config
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    theme: {
-        extend: {},
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    container: {
+      center: true,
     },
-    plugins: [],
+    extend: {
+      fontFamily: {
+        1: ['Montserrat'],
+        2: ['Inter'],
+      },
+      colors: {
+        black1: 'var(--color-black1)',
+        black2: 'var(--color-black2)',
+        black3: 'var(--color-black3)',
+        golden: 'var(--color-golden)',
+        golden2: 'var(--color-golden2)',
+        green: 'var(--color-green)',
+        white: 'var(--color-white)',
+        indigo: 'var(--color-indigo)',
+        amber: 'var(--color-amber)',
+      },
+    },
+  },
+  plugins: [],
 }
