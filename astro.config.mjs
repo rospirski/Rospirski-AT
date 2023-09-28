49315610e6bfb66259451d7e051bbbb786b0af5c
Valdir Rospirski <valdir.rospirski@gmail.com>
2023-09-28 09:28:51 -0300

    Version 1.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from "astro-sitemap";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import sitemap from 'astro-sitemap'
 
-import robotsTxt from "astro-robots-txt";
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rospirski.com/',
-  integrations: [tailwind(), sitemap(), robotsTxt()]
-});
+  integrations: [tailwind(), sitemap(), robotsTxt()],
+  image: {
+    domains: ['rospirski.com'],
+  },
+})
